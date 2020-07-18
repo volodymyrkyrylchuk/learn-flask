ed56d530a0e57e86401406993b4c8e88ca1994be
OOstrovskyi <paragelmen7@gmail.com>
2020-07-18 20:36:44 +0300

    add post method

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -37,3 +38,19 @@ def delete_user(username):
         return f'User {username} was deleted'
     else:
         return 'User doesn`t exist or already deleted'
+
+
+@app.route('/users/add/', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        return_string = f'User {username} was added'
+        if username in storage:
+            return_string = f'User {username} was rewritten'
+        storage[username] = {}
+        return return_string
+
+    return '''<form method="POST">
+                  User name: <input type="text" name="username"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
