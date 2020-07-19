e92268a21bbe698c03b18fc330e3e6026d68946c
Your Name <you@example.com>
2020-07-19 02:15:58 +0100

    "add_new_user" route added. Optionally checking if the user already present in existing dict. If yes, re-adding.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,6 +16,8 @@ storage.update(
         "username4": {}
     }
 )
+
+
 
 
 @app.route('/')
@@ -37,3 +39,20 @@ def delete_user(username):
         return f'User {username} was deleted'
     else:
         return 'User doesn`t exist or already deleted'
+
+
+@app.route('/users/add/', methods=['GET', 'POST'])
+def add_new_user():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        new_user = f'User {username} was added'
+        if username in storage:
+            new_user = f'User {username} was re-added'
+        storage[username] = {}
+        return new_user
+
+    return '''<form method="POST">
+                  User name: <input type="text" name="username"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+app.run()
