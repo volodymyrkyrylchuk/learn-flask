58891c35af546b2591ae0fe19491deb9e70f6625
K-Ihor <i.kucherov.m@gmail.com>
2020-07-22 15:09:42 +0300

    Может я конечно не так сделал как ожидалось но добавить пользователя в словарь если его там нет и проверить есть ли он вроде получилось

--- app.py
+++ app.py
@@ -1,56 +1,84 @@
-from flask import Flask # импортируем из библиотеки flask класс Flask
+from flask import Flask
 
-app = Flask(__name__)  # создаем объект app на основе класса Flask, (__name__) - имя нашего файла
-
-# use Python Dict as DB
-storage = dict()
-# structure of storage: key - username, value - dict(default empty) in future will contain info about current user
+app = Flask(__name__)
 
 
-# put some default users into db
-storage.update(
-    {
+storage = {
         "username1": {},
         "username2": {},
         "username3": {},
         "username4": {}
     }
-)
 
-
-@app.route('/')  # отслеживает URL ('/') - main page)
-def hello_world():
-    return 'Hello, World!'
-
-
-@app.route('/users/list/')
-def user_list():
-    username_list = storage.keys()
-    return '<br>'.join(username_list)
-
-
-@app.route('/users/delete/<username>')
-def delete_user(username):
-    old_users = storage.copy()
-    storage.pop(username, False)
-    if username in old_users:
-        return 'User  was deleted'
+# rout Игорь Кучеров
+@app.route('/<name>')
+def new_user(name):
+    if name in storage:
+        return f'"User" {name} exists in the database'
     else:
-        return 'User doesn`t exist or already deleted'
-
-
-# My route
-@app.route('/users/add/<username>')
-def add_user_list(username):
-    new_users = storage.copy()
-    storage.update({username:{}})
-    if username in new_users:
-        return f'User {username} already in list'
-    else:
-        return f'User {username} added'
+        storage.setdefault(name, {})
+        return storage
 
 
 
 
-if __name__=='__main__': # если запускается через файл app.py, то проект должен запуститься как flask приложение
-    app.run(debug=True)   # запускается локальный сервер
+
+
+
+
+# from flask import Flask # импортируем из библиотеки flask класс Flask
+#
+# app = Flask(__name__)  # создаем объект app на основе класса Flask, (__name__) - имя нашего файла
+#
+# # use Python Dict as DB
+# storage = dict()
+# # structure of storage: key - username, value - dict(default empty) in future will contain info about current user
+#
+#
+# # put some default users into db
+# storage.update(
+#     {
+#         "username1": {},
+#         "username2": {},
+#         "username3": {},
+#         "username4": {}
+#     }
+# )
+#
+#
+# @app.route('/')  # отслеживает URL ('/') - main page)
+# def hello_world():
+#     return 'Hello, World!'
+#
+#
+# @app.route('/users/list/')
+# def user_list():
+#     username_list = storage.keys()
+#     return '<br>'.join(username_list)
+#
+#
+# @app.route('/users/delete/<username>')
+# def delete_user(username):
+#     old_users = storage.copy()
+#     storage.pop(username, False)
+#     if username in old_users:
+#         return 'User  was deleted'
+#     else:
+#         return 'User doesn`t exist or already deleted'
+#
+#
+# # My route
+# @app.route('/users/add/<username>')
+# def add_user_list(username):
+#     new_users = storage.copy()
+#     storage.update({username:{}})
+#     if username in new_users:
+#         return f'User {username} already in list'
+#     else:
+#         return f'User {username} added'
+#
+#
+#
+#
+# if __name__=='__main__': # если запускается через файл app.py, то проект должен запуститься как flask приложение
+#     app.run(debug=True)   # запускается локальный сервер
