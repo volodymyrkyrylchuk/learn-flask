0ac0608b8e58ad787965269f33f6695a14886eb6
Volodymyr Kyrylchuk <vlk@group-fs.com>
2020-07-24 21:19:40 +0300

    feat: answer

--- check_service.py
+++ check_service.py
@@ -5,6 +5,8 @@ new_user = {
     'username': 'Bruce',
 }
 
-response = requests.post('http://localhost:5000/users/add/', json=new_user)
-
-print(response.json())
+# response = requests.post('http://localhost:5000/users/add/', json=new_user)
+#
+# print(response.json())
+response2 = requests.get('http://localhost:5000/users/delete/username2')
+#print(response2.json())
