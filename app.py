d437ecbce0c87b92b31e81e8db46c48f4726821f
OOstrovskyi <paragelmen7@gmail.com>
2020-07-25 14:41:50 +0300

    jsonify delete route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import request
+from flask import Flask  # импортируем из библиотеки flask класс Flask
+from flask import jsonify, request
 
-app = Flask(__name__)
+app = Flask(__name__)  # создаем объект app на основе класса Flask, (__name__) - имя нашего файла
 
 # use Python Dict as DB
 storage = dict()
@@ -11,23 +11,22 @@ storage = dict()
 # put some default users into db
 storage.update(
     {
-        "Kelly": {},
-        "Nelly": {},
-        "Shelly": {},
-        "Belly": {}
+        "username1": {},
+        "username2": {},
+        "username3": {},
+        "username4": {}
     }
 )
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({'msg': 'Hello, World!'})
 
 
 @app.route('/users/list/')
 def user_list():
-    username_list = storage.keys()
-    return '<br>'.join(username_list)
+    return jsonify(storage)
 
 
 @app.route('/users/delete/<username>')
@@ -35,39 +34,25 @@ def delete_user(username):
     old_users = storage.copy()
     storage.pop(username, False)
     if username in old_users:
-        return f'User {username} was deleted'
+        return jsonify(status = 'OK', message = 'User  was deleted')
     else:
-        return 'User doesn`t exist or already deleted'
+        return jsonify(status = 'None', message = 'User doesnt exist or already deleted')
 
 
-# # add user with GET method
-# @app.route('/users/add/<username>')
-# def add_user_get(username):
-#     return_string = f'User {username} was added'
-#     if username in storage:
-#         return_string = f'User {username} was rewritten'
-#     storage[username] = {}
-#     return return_string
+@app.route('/users/add/', methods=["POST"])
+def add_user_list():
+    data = request.get_json()  # get json return dict loaded from json body
+    try:
+        username = data['username']
+    except Exception as e:
+        response = {'msg': f'The field {e} is required'}
+        username = False
+
+    if username:
+        storage[username] = dict()
+        response = {'msg': 'user was added'}
+    return jsonify(response)
 
 
-# add user with POST method
-@app.route('/users/add/', methods=['GET', 'POST'])
-def add_user():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        return_string = f'User {username} was added'
-
-        if username in storage:
-            return_string = f'User {username} was rewritten'
-
-        if username:
-            storage[username] = {}
-        else:
-            return_string = 'Put user name!'
-
-        return return_string
-
-    return '''<form method="POST">
-                  User name: <input type="text" name="username"><br>
-                  <input type="submit" value="Submit"><br>
-              </form>'''
+if __name__ == '__main__':  # если запускается через файл app.py, то проект должен запуститься как flask приложение
+    app.run(debug=True)  # запускается локальный сервер
