4c7d5b99ae720232f47ece418c3e14b742abdef5
Vladimir <vovabelous2012@gmail.com>
2020-10-30 16:27:50 +0200

    adding json response into delete method(hw #3)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request  # импортируем из библиотеки flask класс Flask
-
+from flask import Flask  # импортируем из библиотеки flask класс Flask
+from flask import jsonify, request
 
 app = Flask(__name__)  # создаем объект app на основе класса Flask, (__name__) - имя нашего файла
 
@@ -19,15 +19,14 @@ storage.update(
 )
 
 
-@app.route('/')  # отслеживает URL ('/') - main page)
+@app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({'msg': 'Hello, World!'})
 
 
 @app.route('/users/list/')
 def user_list():
-    username_list = storage.keys()
-    return '<br>'.join(username_list)
+    return jsonify(storage)
 
 
 @app.route('/users/delete/<username>')
@@ -35,20 +34,25 @@ def delete_user(username):
     old_users = storage.copy()
     storage.pop(username, False)
     if username in old_users:
-        return 'User  was deleted'
+        return jsonify(message='User  was deleted')
     else:
-        return 'User doesn`t exist or already deleted'
+        return jsonify (message='User doesnt exist or already deleted')
 
 
-@app.route('/add/<username>', methods=['GET'])
-def add_user(username):
-    if request.method == 'GET':
-        storage.update({username: {}})
-        return f'User {username} added'
-    else:
-        return f'Some error occurred while adding user {username}'
+@app.route('/users/add/', methods=["POST"])
+def add_user_list():
+    data = request.get_json()  # get json return dict loaded from json body
+    try:
+        username = data['username']
+    except Exception as e:
+        response = {'msg': f'The field {e} is required'}
+        username = False
+
+    if username:
+        storage[username] = dict()
+        response = {'msg': 'user was added'}
+    return jsonify(response)
 
 
 if __name__ == '__main__':  # если запускается через файл app.py, то проект должен запуститься как flask приложение
-    app.debug = True
-    app.run()
+    app.run(debug=True)  # запускается локальный сервер
